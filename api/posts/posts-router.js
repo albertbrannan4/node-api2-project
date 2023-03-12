18cadbb0a486a4614d13109255a8ed9a29616893
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-03-12 19:29:34 -0400

    post post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,12 +3,57 @@ const express = require("express");
 const Posts = require("./posts-model");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   Posts.find()
     .then((posts) => {
       res.status(200).json(posts);
     })
-    .catch(() => {});
+    .catch(() => {
+      res.status(500).json({
+        message: "The posts information could not be retrieved",
+      });
+    });
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  Posts.findById(id)
+    .then((post) => {
+      if (!post) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist",
+        });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "The post information could not be retrieved",
+      });
+    });
+});
+
+router.post("/", async (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    Posts.insert({ title, contents })
+      .then(({ id }) => {
+        return Posts.findById(id);
+      })
+      .then((post) => {
+        res.status(201).json(post);
+      })
+      .catch(() => {
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+        });
+      });
+  }
 });
 
 module.exports = router;
