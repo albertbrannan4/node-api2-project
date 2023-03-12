06fa981d6eb3bff19a875ba6be94ba3d2f99a808
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-03-12 05:46:42 -0400

    initial commit getting all posts

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
+const express = require("express");
+const server = express();
+const postsRouter = require("./posts/posts-router");
+
+server.use(express.json());
 // require your posts router and connect it here
+server.use("/api/posts", postsRouter);
+
+server.get("/", (req, res) => {
+  res.send(`
+        <h2>Posts API</h>
+        <p>Welcome to the Posts API</p>
+      `);
+});
+
+module.exports = server;
